f3de5c93f5a136318ebc68954fc157bc69160cbd
Rosenoire <skillwarzaking@gmail.com>
2025-12-18 23:57:36 +0100

    Disabled rendering of the hotbar when in survival or adventure mode.

---
+++ src/main/java/dev/rosenoire/character_engine/mixin/client/hud/InGameHudMixin.java
@@ -0,0 +1,26 @@
+package dev.rosenoire.character_engine.mixin.client.hud;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+    /// Disables the rendering of the hotbar when in survival or adventure mode.
+    @Inject(at = @At("HEAD"), method = "renderHotbar", cancellable = true)
+    private void characterEngine$DisableCharacter(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+        var client = MinecraftClient.getInstance();
+        var player = client.player;
+
+        if (player == null || player.isCreative() || player.isSpectator()) {
+            return;
+        }
+
+        ci.cancel();
+    }
+}
