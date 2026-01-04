035103bf9e5e9f4bff3d42fe607b78725d474818
Rosenoire <skillwarzaking@gmail.com>
2026-01-04 11:16:13 +0100

    Renamed CollectionUtil to CollectionHelper

--- src/main/java/net/collectively/geode/core/util/CollectionHelper.java
+++ src/main/java/net/collectively/geode/core/util/CollectionHelper.java
@@ -1,7 +1,7 @@
 package net.collectively.geode.core.util;
 
 @SuppressWarnings("unused")
-public interface CollectionUtil {
+public interface CollectionHelper {
     static <T> boolean isOutsideOfBounds(T[] array, int index) {
         return index < 0 || index >= array.length;
     }
